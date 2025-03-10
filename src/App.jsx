265b0925d03ebedd4c3c16e4d459b0e29c0bb56e
Ahmad <ahmadzeyad80@outlook.com>
2025-03-10 16:05:57 +0400

    Inializing project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="center">
+        <h1>Hello World !</h1>
+
+      </div>
+      <p>i love hossam 🤫</p>
+    </>
+  )
+}
+
+export default App
